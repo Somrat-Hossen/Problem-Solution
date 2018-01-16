c09ba121b7508ab1e816e6eebffd86b89d5f551c
Somrat-Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2018-01-16 18:51:31 +0600

    Create 4C Registration system.cpp

---
+++ 4C Registration system.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include<iomanip>
+#define PI acos(-1)
+using namespace std;
+
+
+int main()
+{
+  //freopen("in.txt","r",stdin);
+  int n;
+  string str;
+  map<string,int>mp;
+  cin>>n;
+
+  while(n--)
+  {
+      cin>>str;
+     if(mp[str]==0)
+     {
+       puts("OK");
+       mp[str]++;
+     }
+     else
+     {
+       cout<<str<<mp[str]<<endl;
+       mp[str]++;
+     }
+  }
+    return 0;
+}
