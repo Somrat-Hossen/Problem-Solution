192b92d67d13499986468c0f886a738611c8c359
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2018-02-23 00:39:37 +0600

    Happy

---
+++ UVa/11799 Horror Dash.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#include <stdlib.h>
+#include<iomanip>
+
+#define maxx 1e+5
+#define PI            acos(-1.0)
+#define pb            push_back
+#define all(x)        (x).begin(), (x).end()
+#define clr(ar)        memset(ar, 0, sizeof(ar))
+#define loop(i,n)      for(int i=0; i<n; i++)
+#define maxint(a,b,c) (a>b)?(a>c)?a:c :(b>c)?b:c
+#define minint(a,b,c) (a<b)?(a<c)?a:c :(b<c)?b:c
+#define READ          freopen("in.txt", "r", stdin);
+#define WRITE          freopen("out.txt", "w", stdout);
+
+using namespace std;
+
+typedef long long int ll;
+typedef  set<int>             si;
+typedef  map<string,int>      mst;
+typedef  map<int,int>	      mii;
+typedef  map<char,int>        mci;
+typedef  map<int,string>	   mis;
+typedef  vector<long long> 	   vi;
+typedef  vector<string> 	   vs;
+int n,i,x,y,z,j,len,cnt(0),sum=0,a,b,c,temp,d,res=0;
+int arr[100000];
+int dp[1000];
+int two[1000][1000];
+string str,st;
+
+int check(int n)
+{
+
+}
+
+int main()
+{
+    int tc;
+    cin>>tc;
+    while(tc--)
+    {
+      cin>>n;
+      loop(i,n)
+      {
+          cin>>arr[i];
+      }
+      sort(arr,arr+n,greater<int>());
+      cout<<"Case "<<++res<<": "<<arr[0]<<endl;
+    }
+
+    return 0;
+}
+
