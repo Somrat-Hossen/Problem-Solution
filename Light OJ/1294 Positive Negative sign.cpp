5495344d08e709103189937f608b2abb033a9a5e
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2018-04-04 15:09:57 +0600

    Create 1294 Positive Negative sign.cpp

---
+++ Light OJ/1294 Positive Negative sign.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+    {
+      long long int n,m,sign,sum,t,i,j=1;
+       cin>>t;
+       while(t--)
+       {
+           sign=-1;sum=0;
+           cin>>n>>m;
+           printf("Case %lld: %lld\n",j,(n/2)*m);
+           j++;
+             }
+        return 0;
+    }
