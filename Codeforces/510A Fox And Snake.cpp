7fa85c995193395f746af89e831684ab2bd24656
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2018-05-24 16:46:09 +0600

    Add files via upload

---
+++ Codeforces/510A Fox And Snake.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#include <stdlib.h>
+#include<iomanip>
+
+#define maxx 1e+5
+#define PI            acos(-1.0)
+#define pb            push_back
+#define all(x)        (x).begin(), (x).end()
+#define clr(ar)        memset(ar, 0, sizeof(ar))
+#define loop(i,n)      for(int i=0; i<n; i++)
+#define lop(i,n)      for(int i=1; i<=n; i++)
+#define maxint(a,b,c) (a>b)?(a>c)?a:c :(b>c)?b:c
+#define minint(a,b,c) (a<b)?(a<c)?a:c :(b<c)?b:c
+#define READ          freopen("in.txt", "r", stdin);
+#define WRITE          freopen("out.txt", "w", stdout);
+
+using namespace std;
+
+typedef long long int ll;
+typedef  set<int>             si;
+typedef  map<string,int>      mst;
+typedef  map<int,int>	      mii;
+typedef  map<char,int>        mci;
+typedef  map<int,string>	   mis;
+typedef  vector<long long> 	   vi;
+typedef  vector<string> 	   vs;
+ll n,i,x,y,z,j,len,cnt(0),sum=0,a,b,c,temp,d,res=0,k,tc,m=1000003,t,r;
+int arr[100000],ar[1000];
+int dp[1000];
+ll two[1000][1000];
+int dx[4]= {0,0,1,-1};
+int dy[4]= {1,-1,0,0};
+string str,st;
+queue<int>q;
+vector<int>vec;
+char ch;
+
+
+int main()
+ {
+     a=2;
+     cin>>n>>m;
+     for(i=1;i<=n;i++)
+     {
+         for(j=1;j<=m;j++)
+         {
+             if(i%2) {cout<<"#";}
+          else
+          {
+             if(i==a && j==m)
+             {
+                 cout<<"#";
+                 a+=4;
+             }
+             else if(i%4==0 && j==1)
+             {
+                 cout<<"#";
+             }
+             else
+                cout<<".";
+          }
+
+       }cout<<endl;
+     }
+
+    return 0;
+  }
+
