a237f2b9c5b12c0f2794ba54878b2c0972577d34
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2018-09-09 19:30:59 +0600

    Update 552B Vanya and Books.cpp

--- Codeforces/552B Vanya and Books.cpp
+++ Codeforces/552B Vanya and Books.cpp
@@ -45,26 +45,64 @@ int check(int ,int)
 int main()
  {
      cin>>n;
-     if(n<10)cout<<n<<endl;
+     if(n<10){cout<<n<<endl; return 0;}
 
-     else
+      else if(n>=10 and n<=99)
         {
-            for(i=1;i<=n;i++)
-            {
-             x=i;cnt=0;
-             while(x)
-             {
-                x/=10;
-                cnt++;
-             }
-           res+=cnt;
-            }
+            res+=(n-9)*2+9;
 
+        }
+        else if(n>=100 and n<=999)
+        {
+            res+=(n-99)*3+189;
+
+        }
+        else if(n>=1000 and n<=9999)
+        {
+            res+=(n-999)*4+2889;
+
+        }
+        else if(n>=10000 and n<=99999)
+        {
+            res+=(n-9999)*5+38889;
+
+        }
+        else if(n>=100000 and n<=999999)
+        {
+            res+=(n-99999)*6+488889;
+
+        }
+        else if(n>=1000000 and n<=9999999)
+        {
+            res+=(n-999999)*7+5888889;
+
+        }
+        else if(n>=10000000 and n<=99999999)
+        {
+            res+=(n-9999999)*8+68888889;
+
+        }
+        else if(n>=100000000 and n<=999999999)
+        {
+            res+=(n-99999999)*9+788888889;
+
+        }
+        else
+        {
+            res=8888888889+10;
         }
         cout<<res<<endl;
 
 
     return 0;
   }
+
+
+
+
+
+
+
+
 
 
