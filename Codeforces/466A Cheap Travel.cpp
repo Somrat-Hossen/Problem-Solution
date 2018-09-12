899446f674d15fd303eb24a7054e33190ec83f5e
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2018-09-12 14:45:40 +0600

    Add files via upload

---
+++ Codeforces/466A Cheap Travel.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+#include <stdlib.h>
+#include<iomanip>
+
+#define maxx 1e+5
+#define PI            acos(-1.0)
+#define pb            push_back
+#define all(x)        (x).begin(), (x).end()
+#define clr(ar)        memset(ar, 0, sizeof(ar))
+#define loop(i,n)      for(int i=0; i<n; i++)
+#define lop(i,n)      for(int i=1; i<=n; i++)
+#define maxint(a,b,c) (a>b)?(a>c)?a:c :(b>c)?b:c
+#define minint(a,b,c) (a<b)?(a<c)?a:c :(b<c)?b:c
+#define READ          freopen("in.txt", "r", stdin);
+#define WRITE          freopen("out.txt", "w", stdout);
+
+using namespace std;
+
+typedef long long int ll;
+typedef  set<int>             si;
+typedef  map<string,int>      mst;
+typedef  map<int,int>	      mii;
+typedef  map<char,int>        mci;
+typedef  map<int,string>	   mis;
+typedef  vector<long long> 	   vi;
+typedef  vector<string> 	   vs;
+map<int,int>mp;
+map<int,int>::iterator it;
+ll n,i,x,y,z,j,len,cnt(0),sum=0,a,b,c,temp,d,res=0,k,tc,mod=1000003,t,r,pos,ans=0,m;
+int arr[100000],ar[1000];
+int dp[1000];
+ll two[1000][1000];
+int dx[4]= {0,0,1,-1};
+int dy[4]= {1,-1,0,0};
+string str,st;
+queue<int>q;
+vector<int>vec;
+char ch;
+
+int check(int ,int)
+{
+
+}
+
+int main()
+ {
+     cin>>n>>m>>a>>b;
+
+    if(m*a<=b)
+      {
+        cout<<n*a<<endl;
+      }
+    else
+     {
+        a = ( (n/m)*b + (n%m)*a );
+        b = ( (n/m)*b + ((n%m==0)?0:b)) ;
+        ans = min(a,b);
+        cout << ans << endl;
+
+     }
+
+
+
+
+
+
+    return 0;
+  }
+
+
+
+
+
+
+
+
