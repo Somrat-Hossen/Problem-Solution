316b83d4d1098ff3f9ec65ee3d8fe356e4c914bd
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2018-09-16 12:39:47 +0600

    Add files via upload

---
+++ Codeforces/75A Life Without Zeros.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+#include <stdlib.h>
+#include<iomanip>
+
+#define maxx 1e+5
+#define PI            acos(-1.0)
+#define pb            push_back
+#define all(x)        (x).begin(), (x).end()
+#define clr(ar)        memset(ar, 0, sizeof(ar))
+#define loop(i,n)      for(int i=0; i<n; i++)
+#define lop(i,n)      for(int i=1; i<=n; i++)
+#define maxint(a,b,c) (a>b)?(a>c)?a:c :(b>c)?b:c
+#define minint(a,b,c) (a<b)?(a<c)?a:c :(b<c)?b:c
+#define READ          freopen("in.txt", "r", stdin);
+#define WRITE          freopen("out.txt", "w", stdout);
+
+using namespace std;
+
+typedef long long int ll;
+typedef  set<int>             si;
+typedef  map<string,int>      mst;
+typedef  map<int,int>	      mii;
+typedef  map<char,int>        mci;
+typedef  map<int,string>	   mis;
+typedef  vector<long long> 	   vi;
+typedef  vector<string> 	   vs;
+map<int,int>mp;
+map<int,int>::iterator it;
+ll n,i,x,y,z,j,len,cnt(0),sum=0,a,b,c,res=0,k,tc,mod=1000003,t,r,pos,ans=0,m;
+int arr[100000],ar[22];
+int dp[1000];
+ll two[1000][1000];
+int dx[4]= {0,0,1,-1};
+int dy[4]= {1,-1,0,0};
+string str,st;
+queue<int>q;
+vector<int>vec;
+char ch;
+
+int check(int ,int)
+{
+
+}
+
+int main()
+ {
+     unsigned long long int tmp,d=0,tot;
+       x=0;
+       cin>>n>>m;
+       tot=n+m;
+
+       while(n)
+       {
+           k=n%10;
+           if(k!=0)
+           {
+               ar[++x]=k;
+           }
+           n/=10;
+       }
+       for(i=x;i>=1;i--)
+       {
+         res=(res*10)+ar[i];
+       }
+      // cout<<res<<endl;
+       x=0;
+       while(m)
+       {
+           k=m%10;
+           if(k!=0)
+           {
+               ar[++x]=k;
+           }
+           m/=10;
+       }
+       for(i=x;i>=1;i--)
+       {
+           sum=(sum*10)+ar[i];
+       }
+     // cout<<sum<<endl;
+       tmp=res+sum;
+       x=0;;
+       while(tot)
+       {
+           k=tot%10;
+           if(k!=0)
+           {
+               ar[++x]=k;
+           }
+           tot/=10;
+       }
+       for(i=x;i>=1;i--)
+       {
+           d=(d*10)+ar[i];
+           //cout<<d<<" ";
+       }
+     cout<<((tmp==d)?"YES":"NO")<<endl;
+    // cout<<tmp<<" "<<d;
+
+
+
+
+    return 0;
+  }
+
+
