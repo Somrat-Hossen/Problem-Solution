16bb3f5e9f839879fd9121aa76a7cda5e8a374a8
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2018-09-22 14:37:39 +0600

    Add files via upload

---
+++ Codeforces/1047A  Little C Loves 3 I.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+ int i,n,w,res,weight,price,cur,m,t,x,j,k;
+ int a[55],b[55];
+
+int main()
+{
+  cin>>n;
+ if(n%3!=2)
+ {
+    cout<<1<<" "<<1<<" "<<n-2;
+ }
+
+ else
+ {
+  cout<<1<<" "<<2<<" "<<n-3;
+ }
+
+
+
+ return 0;
+}
+
+
+
+
+
