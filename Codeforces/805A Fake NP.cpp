c2c20caf7fd56a4712a2f838e98aa9a1f87f7aa1
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2018-09-27 18:26:52 +0600

    Add files via upload

---
+++ Codeforces/805A Fake NP.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <stdlib.h>
+#include<iomanip>
+
+typedef unsigned long long ll;
+using namespace std;
+
+ll m,n;
+
+int main()
+ {
+    cin>>n>>m;
+
+    if(m==n) cout<<n<<endl;
+
+    else  cout<<2<<endl;
+
+
+
+
+    return 0;
+  }
+
+
