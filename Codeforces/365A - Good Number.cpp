dbcbc743ece6e1598bd943fb3ca6d7a764840481
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2018-10-07 22:46:22 +0600

    Add files via upload

---
+++ Codeforces/365A - Good Number.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+#include <stdlib.h>
+#include<iomanip>
+
+#define maxx 1e+5
+#define PI            acos(-1.0)
+#define pb            push_back
+#define all(x)        (x).begin(), (x).end()
+#define clr(ar)        memset(ar, 0, sizeof(ar))
+#define loop(i,n)      for(int i=0; i<n; i++)
+#define lop(i,n)      for(int i=1; i<=n; i++)
+#define maxint(a,b,c) (a>b)?(a>c)?a:c :(b>c)?b:c
+#define minint(a,b,c) (a<b)?(a<c)?a:c :(b<c)?b:c
+#define READ          freopen("in.txt", "r", stdin);
+#define WRITE          freopen("out.txt", "w", stdout);
+
+#define Check(r,c) ((r>=0 && r<=7) && (c>=0 && c<=7))
+#define Check(a,b,c) ((a>b)?(a>c)?a:b: (b>c)?b:c)
+
+using namespace std;
+
+typedef long long int ll;
+typedef  set<int>             si;
+typedef  map<string,int>      mst;
+typedef  map<int,int>	      mii;
+typedef  map<char,int>        mci;
+typedef  map<int,string>	   mis;
+typedef  vector<long long> 	   vi;
+typedef  vector<string> 	   vs;
+map<int,int>mp;
+map<int,int>::iterator it;
+ll n,i,x,y,z,j,len,cnt(0),sum=0,a,b,c,temp,d,res=0,k,tc,mod=1000003,t,r,pos,ans=0,m;
+int arr[100000],ar[1000];
+int dp[1000];
+ll two[1000][1000];
+int dx[4]= {0,0,1,-1};
+int dy[4]= {1,-1,0,0};
+string str,st;
+queue<int>q;
+vector<int>vec;
+char ch;
+
+int check(int ,int)
+{
+
+
+
+}
+
+int main()
+ {
+     si st;
+     cin>>n>>m;
+     loop(i,n)
+     {
+         cin>>arr[i];
+     }
+    // if(m==0) {cout<<0<<endl; return 0;}
+
+     loop(i,n)
+     {
+         st.clear();
+         cnt=0;
+        x=arr[i];
+        while(x)
+        {
+            y=x%10;
+            if(y>=0 and y<=m)
+            {
+                st.insert(y);
+            }
+
+            x/=10;
+            //if(m==0 and y==0 and !res) res++;
+        }
+        if(st.size()==m+1) res++;
+     }
+
+     cout<<res<<endl;
+
+
+
+    return 0;
+  }
+
+
