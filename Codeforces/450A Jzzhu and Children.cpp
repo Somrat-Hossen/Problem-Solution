6862f594e4752dcdeb52dc2f3adecbb6de4cd9f4
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2018-11-30 18:56:14 +0600

    Add files via upload

---
+++ Codeforces/450A Jzzhu and Children.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <stdlib.h>
+#include<iomanip>
+
+using namespace std;
+
+typedef unsigned int ui;
+typedef unsigned long long int ull;
+
+int main()
+ {
+   int i,n,m,x,y;
+   queue<int>value;
+   queue<int> index;
+   cin>>n>>m;
+
+   for(i=1;i<=n;i++)
+   {
+     cin>>x;
+      value.push(x);
+      index.push(i);
+   }
+
+   while(value.size()!=1)
+   {
+      x=value.front();
+      y=index.front();
+      if(x<=m)
+      {
+        value.pop();
+        index.pop();
+      }
+      else
+      {
+        value.push(x-m);
+        index.push(y);
+        value.pop();
+        index.pop();
+
+      }
+   }
+
+   cout<<index.front()<<endl;
+
+
+
+
+    return 0;
+  }
+
