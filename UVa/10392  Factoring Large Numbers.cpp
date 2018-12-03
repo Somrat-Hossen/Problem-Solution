5722d60a86ca85f5b4c42527f9543d91ea43c7bb
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2018-12-03 20:45:50 +0600

    Add files via upload

---
+++ UVa/10392  Factoring Large Numbers.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#define mx 10000000
+
+
+using namespace std;
+
+typedef  long long int ll;
+
+ll num,sq,x,i,j,cnt,len,p;
+bool check[mx+1];
+vector<ll> prime;
+
+void prime_test()
+{
+  prime.push_back(2);
+   p=sqrt(mx)+1;
+
+ for(i=4;i<=mx;i+=2)///All even except 3 are marked.
+  {
+     check[i]=1;
+  }
+    for(int i=3;i<=mx;i+=2)///same sqrt(n)==i*i
+    {
+      if(check[i]==0)
+      {
+          prime.push_back(i); ///i means prime
+          if(i<=p)  ///there is no divisor after sqrt n
+          {
+            for(int j=i*2;j<=mx;j+=i)
+           {
+              check[j]=1; ///0 means not prime
+           }
+          }
+      }
+    }
+}
+
+
+
+int main()
+{
+  prime_test();
+
+  /*for(i=0;i<50;i++)
+  {
+    cout<<prime[i]<<" ";
+  }*/
+
+ while(scanf("%lld",&num)&& num>0)
+  {
+      for(i=0;prime[i]*prime[i]<=num;i++)
+       {
+        while(num%prime[i]==0 && num)
+        {
+        printf("    %lld\n",prime[i]);
+      //  cout<<"    "<<prime[i]<<endl;
+          num/=prime[i];
+        }
+       }
+       if(num>2)  printf("    %lld\n",num);
+       printf("\n");
+
+  }
+
+
+    return 0;
+}
+
