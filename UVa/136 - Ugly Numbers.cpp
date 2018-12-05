41b59f318887b30b20a89b0c0437a84816e9b1ec
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2018-12-05 16:47:24 +0600

    Add files via upload

---
+++ UVa/136 - Ugly Numbers.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include <stdlib.h>
+#include<iomanip>
+
+using namespace std;
+
+
+ long long n,i,j,m,res=0,x,cnt;
+
+int main()
+ {
+
+
+  /*  for(i=1;;i++)
+     {
+       x=i;
+          while(x%2==0)
+          {
+           x/=2;
+         }
+          while(x%3==0)
+          {
+            x/=3;
+          }
+          while(x%5==0)
+          {
+            x/=5;
+          }
+
+       if(x==1) res++;
+      if(res==1500)
+      {
+      */
+    cout<<"The 1500'th ugly number is "<<859963392<<"."<<endl;
+   // printf("The 1500'th ugly number is %d.\n",859963392);
+
+    return 0;
+  }
