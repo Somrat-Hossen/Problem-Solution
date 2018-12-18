f68a57a3630a86731c5b924778c99e7333db4538
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2018-12-19 00:46:39 +0600

    Add files via upload

---
+++ Essential Problem/Next permutation.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+int main()
+{
+   int n;
+   vector<int>vec;
+   cin>>n;
+
+   for(int i=0;i<n;i++)
+   {
+      vec.push_back(i+1);
+   }
+
+
+   do
+   {
+     for(int i=0;i<n;i++)
+     {
+        cout<<vec[i]<<" ";
+     }
+     cout<<endl;
+    }while( next_permutation( vec.begin(), vec.end() ) );
+
+
+
+    return 0;
+}
+
+
+
+
