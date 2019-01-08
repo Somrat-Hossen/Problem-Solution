4d0fecb34d245a9d6703b9a2110d5953bb001309
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2019-01-08 21:45:29 +0600

    Add files via upload

---
+++ Essential Problem/Logarithm base conversion.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int n;
+   cout<<(log2(75)/log2(3))<<endl;
+   cout<<log(16)<<endl; /// loge base or natural log
+   cout<<log2(16)<<endl; /// 2 base log
+   cout<<floor(log10(1235))+1<<endl; ///count digit.
+  return 0;
+}
