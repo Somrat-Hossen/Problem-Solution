af9ad48f4e4e356327e1080fb239ba421216d071
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2019-10-03 23:47:05 +0600

    Add files via upload

--- Spoj/Dquery.cpp
+++ Spoj/Dquery.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 typedef long long ll;
 
-int currL = 0,currR =-1;
+int currL = 0,currR =0;
 int cnt = 0;
 int res[2*100005],arr[mx],vis[1000005];
 
@@ -13,11 +13,12 @@ int block;
 struct Query
 {
 	int L, R,id;
-};
+}q[200005];
 
 bool compare(Query x,Query y)
 {
-
+    /// of the same block are arranged together and within a block,
+     /// queries are sorted in increasing order of R values.
     if (x.L/block != y.L/block)  /// Different blocks, sort by block.
       return x.L/block < y.L/block;
 
@@ -35,8 +36,7 @@ void Remove(int x)
   if(vis[arr[x]]==0) cnt--;
 }
 
-
-void queryResults(int arr[], int n, Query q[], int m)
+void queryResults(int n, int m)
 {
     block = (int)sqrt(n);
     sort(q, q + m, compare);
@@ -51,10 +51,10 @@ void queryResults(int arr[], int n, Query q[], int m)
     {
         int l = q[i].L, r = q[i].R;
 
-      while(currL > l) Add(--currL);
-      while(currR < r) Add(++currR);
       while(currL < l) Remove(currL++);
-      while(currR > r) Remove(currR--);
+      while(currL > l) Add(currL-1),currL--;
+      while(currR <= r) Add(currR++);
+      while(currR > r+1) Remove(currR-1),currR--;
 
      res[q[i].id]=cnt;
 
@@ -66,31 +66,33 @@ int main()
 {
     //freopen("in.txt","r",stdin);
 
-    ios_base::sync_with_stdio(false);
+   ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 	cerr.tie(NULL);
 
-	Query q[2*100005];
-	int n,m;
 
-	scanf("%d",&n);
-	for(int i=1;i<=n;i++) scanf("%d",&arr[i]);
+   int n,m;
+	cin>>n;
 
-	scanf("%d",&m);
+	for(int i=0;i<n;i++) cin>>arr[i];
 
-	for(int i=0;i<m;i++)
-	{
-	  scanf("%d %d",&q[i].L,&q[i].R);
-	  q[i].id=i;
-	}
-
-	queryResults(arr, n, q, m);
+	cin>>m;
 
 	for(int i=0;i<m;i++)
-	{
-	   printf("%d\n",res[i]);
-	}
+   {
+      cin>>q[i].L>>q[i].R;
+      q[i].id=i;
+      q[i].L--;
+      q[i].R--;
+   }
+
+   queryResults(n,m);
+
+   for(int i=0;i<m;i++)
+   {
+      cout<<res[i]<<endl;
+   }
 
   return 0;
 }
