7a9471fe388901f256b66eac8fcdf537c6032d40
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2019-11-08 12:53:17 +0600

    Add files via upload

---
+++ Codeforces/1243C Tile Painting.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define mx 1000000
+typedef long long ll;
+using namespace std;
+
+int main()
+{
+
+   ///freopen("in.txt","r",stdin);
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cerr.tie(NULL);
+
+	ll a,tmp,num,q;
+
+
+        cin>>num;
+
+        int limit=sqrt(num);
+
+        for(int i=2;i<=limit;i++)
+        {
+           if(num%i==0)
+           {
+               while((num%i==0))
+               {
+                 num/=i;
+               }
+               if(num==1) cout<<i;
+               else cout<<1;
+               return 0;
+           }
+        }
+        cout<<num<<endl;///if the num is prime
+
+    return 0;
+}
+
