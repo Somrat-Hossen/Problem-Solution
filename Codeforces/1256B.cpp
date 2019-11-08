fba8d2cc6b0d7b6846595294e7b5b4ad3fb3c764
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2019-11-08 23:53:11 +0600

    Add files via upload

---
+++ Codeforces/1256B.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#define mx 1000000
+typedef long long ll;
+using namespace std;
+
+
+
+ int n,i,j,m,x,y,z;
+
+
+int arr[105];
+
+int main()
+{
+
+   ///freopen("in.txt","r",stdin);
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cerr.tie(NULL);
+
+	int a,b,c,d,q;
+
+	  cin>>q;
+
+	  while(q--)
+     {
+        cin>>n;
+        for(int i=1;i<=n;i++)
+        {
+           cin>>arr[i];
+        }
+
+
+          int tmp=n-1,need=1,cnt=0;
+          int vis[105]={0};
+
+          for(int i=1;i<=n && cnt<tmp;i++)
+          {
+             for(int j=1;j<=n && cnt<tmp;j++)
+             {
+                if(arr[j]==need)
+                {
+                    int k=j;
+                     while( (arr[k]<arr[k-1]) && (k>1) && (cnt<tmp) && (vis[k-1]==0))
+                     {
+                        swap(arr[k],arr[k-1]);
+                        vis[k-1]=1;
+                        cnt++;
+                        k--;
+                     }
+                     need++;
+                }
+             }
+          }
+
+          for(int i=1;i<=n;i++) cout<<arr[i]<<" ";cout<<endl;
+     }
+
+    return 0;
+}
+
