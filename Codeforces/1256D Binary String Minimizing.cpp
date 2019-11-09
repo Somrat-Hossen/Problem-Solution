f7b1639a6fd423271c838ac85355043ce0ee0077
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2019-11-09 16:52:25 +0600

    Add files via upload

---
+++ Codeforces/1256D Binary String Minimizing.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define mx 1000000
+typedef long long ll;
+using namespace std;
+
+
+
+
+
+int main()
+{
+
+   ///freopen("in.txt","r",stdin);
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cerr.tie(NULL);
+
+    ll n,i,j,m,x,y,z,q,k;
+
+      cin>>q;
+
+      while(q--)
+      {
+            cin>>n>>k;
+            string str;
+
+            ll j=0;
+
+            cin>>str;
+
+            for(ll i=0,len=str.size() ;i<len &&k>0 ;i++)
+            {
+                 if(str[i]=='0')
+                 {
+                    ll dis=min(k,i-j);///find the dis to place the zero
+                    swap(str[i],str[i-dis]); ///swap 0 to his  possible position
+                    j++;   ///keep track the last swapped pos of 0
+                    k-=dis;///dec the num of swap from the total operation
+
+                 }
+            }
+            cout<<str<<endl;
+      }
+
+
+
+
+    return 0;
+}
+
