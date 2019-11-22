7fd8177191de9e40bce0aa77f6ecb783101fc485
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2019-11-22 19:21:06 +0600

    Add files via upload

---
+++ Essential Problem/Equal or less element using binary search.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#define mx 100005
+typedef long long ll;
+using namespace std;
+
+
+
+
+int arr[mx];
+
+int Binary_search(int x,int l,int r)
+{
+
+   int pos=-1;
+
+     while(l<=r)
+     {
+        int mid=(l+r)/2;
+        if(arr[mid]<=x) {pos= mid;l=mid+1;}///find rightmost element
+        ///if we want leftmost element then set r=mid-1;
+
+        else if(arr[mid]>x) r=mid-1;
+        else l=mid+1;
+     }
+     return pos;
+}
+
+int main()
+{
+
+   ///freopen("in.txt","r",stdin);
+   ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cerr.tie(NULL);
+
+	int n,item;
+
+   cin>>n>>item;
+
+   for(int i=0;i<n;i++) cin>>arr[i];
+
+
+   sort(arr,arr+n);
+   //for(int i=0;i<n;i++) cout<<arr[i];
+
+   int position=Binary_search(item,0,n-1);
+   cout<<position;
+
+
+    return 0;
+}
+
+
--- Essential Problem/How many ways we can devide an array into 3 contiguous part whose sum is equal.cpp
+++ Essential Problem/How many ways we can devide an array into 3 contiguous part whose sum is equal.cpp
@@ -1,1 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ll n,a,s,sum[500005],c=0,cnt=0,i;
+
+int main()
+{
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+      cin>>a;
+      sum[i]=sum[i-1]+a;
+    }
+
+    if(sum[n]%3){cout<<0;return 0;}
+
+    s=sum[n]/3;
+    for(i=1;i<n;i++)///if we got an element s/3 at last part then it will not counted untill got two part(e.g 4 5 3.. )
+    {
+       if(sum[i]==s*2)cnt+=c;
+       if(sum[i ]==s)c++;
+    }
+
+    cout<<cnt<<endl;
+
+    return 0;
+}
 
