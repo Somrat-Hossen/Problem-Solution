92118fa48fa9669f92c81ce391a2439a1544e512
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2019-12-01 22:27:18 +0600

    Add files via upload

---
+++ Codeforces/1263B.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#define mx 100005
+typedef long long ll;
+using namespace std;
+
+
+
+
+set<string>st;
+
+ char Change(string str)
+ {
+    for(int i=0;i<=9;i++)
+    {
+       str[0]=char(i+'0'); ///if changing string prevously in our set then it is accepted ,else try next char
+       if(st.find(str)==st.end()) return (str[0]);
+    }
+ }
+
+int main()
+{
+
+   ///freopen("in.txt","r",stdin);
+   ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cerr.tie(NULL);
+
+   string str[11];
+   int q,n;
+
+
+
+   cin>>q;
+   while(q--)
+   {
+      cin>>n;
+
+      for(int i=0;i<n;i++) cin>>str[i],st.insert(str[i]);
+      int need=n-st.size();
+
+      if(need==0) goto print;///skip change
+
+      for(int i=0;i<n-1;i++)
+      {
+         for(int j=i+1;j<n;j++)
+         {
+            if(str[i]==str[j])
+            {
+               str[j][0]= Change(str[j]);
+               //break;
+               st.insert(str[j]);///include newly changed string into our list
+
+            }
+         }
+      }
+
+      print:
+      cout<<need<<endl;
+      for(int i=0;i<n;i++) cout<<str[i]<<endl;
+      st.clear();
+   }
+
+
+
+    return 0;
+}
+
+
