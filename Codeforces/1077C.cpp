2cf7cbf947bb06ae2dcf458ccb54e6bb7fe23bed
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2019-12-04 13:52:05 +0600

    Add files via upload

---
+++ Codeforces/1077C.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define mx 200005
+typedef long long ll;
+using namespace std;
+
+
+
+ int n,i,j,m,x,y,z,q;
+ int arr[mx];
+ int neww[mx];
+ ll sum,res;
+ string str;
+
+
+int main()
+{
+
+   ///freopen("in.txt","r",stdin);
+   ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cerr.tie(NULL);
+
+	cin>>n;
+
+	for(int i=1;i<=n;i++)
+   {
+      cin>>arr[i];
+      sum+=arr[i];
+      neww[i]=arr[i];///copy to the neww array
+   }
+   sort(neww+1,neww+1+n);
+   int firstmx,secondmx;
+
+   firstmx=neww[n],secondmx=neww[n-1];///we need just first or second maximum
+
+   for(int i=1;i<=n;i++)
+   {
+      ll current=arr[i],large=firstmx;
+      if(current==neww[n]) large=secondmx;
+
+      if( (sum- (current+large ))==large) res++,neww[res]=i;
+   }
+   cout<<res<<endl;
+   for(int i=1;i<=res;i++) cout<<neww[i]<<" ";cout<<endl;
+
+
+    return 0;
+}
---
+++ Codeforces/1255B.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define mx 200005
+typedef long long ll;
+using namespace std;
+
+
+
+ int n,i,j,m,x,y,z,q;
+ int arr[mx];
+ ll csum[mx];
+ string str;
+
+
+int main()
+{
+
+   ///freopen("in.txt","r",stdin);
+   ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cerr.tie(NULL);
+
+	cin>>q;
+	while(q--)
+   {
+      cin>>n>>m;
+     ll sum=0;
+      for(int i=0;i<n;i++) cin>>arr[i],sum+=arr[i];
+      if( (n==2) || m<n) cout<<-1<<endl;
+      else
+      {
+         cout<<(sum*2)<<endl;
+
+         int i=1,j=2;
+         for(int k=1;k<m;k++)
+         {
+            cout<<i<<" "<<j<<endl;
+            i++,j++;
+         }
+         cout<<1<<" "<<m<<endl;
+
+      }
+
+   }
+
+
+    return 0;
+}
+
