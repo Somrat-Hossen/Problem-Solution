2e2e636e2e880fa031d9fb90f0867c426a1ba99e
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2019-12-05 17:57:20 +0600

    Update 1083 Histogram.cpp

--- Light OJ/1083 Histogram.cpp
+++ Light OJ/1083 Histogram.cpp
@@ -1,106 +1,112 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define mx 30005
+typedef long long ll;
 using namespace std;
 
-int n,maxArea,min_elem,length,area ;
-
-
-/// This algo complexity is o(n^2);
 /*
-int Largest_area(int hist[],int n)
-{
+Idea:
+ Given an array representing height of bar in bar graph, find max histogram
+ * area in the bar graph. Max histogram will be max rectangular area in the
+ * graph.
+ *
+ * Maintain a stack
+ *
+ * If stack is empty or value at index of stack is less than or equal to value at current
+ * index, push this into stack.
+ * Otherwise keep removing values from stack while stack top is greater than the cur index value
+ * While removing value from stack calculate area
+ * if stack is empty
+ * it means that till this point value just removed has to be smallest element
+ * so area = arr[top] * i
+ * if stack is not empty then this value at index top is less than or equal to
+ * everything from stack top + 1 till i. So area will
+ * area = arr[top] * (i - stack.top() - 1);
+ * Finally maxArea is area if area is greater than maxArea.
 
-     for (int i = 0; i <n; ++i)
-     {
-        int h = hist[i];
-
-        maxArea =max(maxArea, h);
-
-        for (int j = i - 1; j >= 0; --j)
-        {
-
-            h =min(h, hist[j]);
-             int w = (i - j + 1);
-
-            maxArea =max(maxArea, h * w);
-        }
-     }
-
-  return maxArea;
-
-}
 */
 
-int Largest_area(int hist[],int n)
+
+void Get_max_area(int arr[],int n)
 {
-      int i=0;
-     stack<int>st;
-     maxArea=0;
+   stack<int>stk;
+   ll max_area=0,area;
+   int top,i;
 
-    while ( i < n)
-    {
-        if(st.empty() || (hist[st.top()] <= hist[i]))
-        {
-           st.push(i);
-           i++;        /// we need to pop elem untill st top is equal <=hist[i] ,whenever st top<=hist[i]
-                       /// so i increase only here not another place ,think a little bit
+   for( i=0;i<n;i++)
+   {
+      if( (stk.empty()) || (arr[stk.top()]<=arr[i]) ) stk.push(i);
+      else
+      {
+         while( !stk.empty() && (arr[stk.top()]>arr[i]))
+         {
+             top=stk.top();stk.pop();
+
+             if(stk.empty()) area=arr[top]*i;
+             else area=arr[top]*((i-stk.top()-1));
+
+             max_area=max(max_area,area);
          }
+        stk.push(i);
+      }
+   }
 
-        else
-        {
-           int top=st.top();
-           st.pop();
-           if(st.empty())
-             {
-               area=hist[top]*i;
-             }
-             else
-             {
-               area=hist[top]*(i-st.top()-1);
-             }
-           maxArea=max(maxArea,area);
-        }
-     }
+   while( !stk.empty() ) ///if last index element is greater...........
+   {
+       top=stk.top();stk.pop();
 
-     while(!st.empty())
-     {
-         int top=st.top();
-         st.pop();
+       if(stk.empty()) area=arr[top]*i;
+       else area=arr[top]*((i-stk.top()-1));
 
-           if(st.empty())
-             {
-               area=hist[top]*i;
-             }
-             else
-             {
-               area=hist[top]*(i-st.top()-1);
-             }
-           maxArea=max(maxArea,area);
-     }
-     return maxArea;
+       max_area=max(max_area,area);
+   }
+
+   cout<<max_area<<endl;
+
 }
+
 
 int main()
 {
 
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    cerr.tie(NULL);
+   ///freopen("in.txt","r",stdin);
+   ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cerr.tie(NULL);
 
-   int tc;
-   cin>>tc;
+	/*
+	 o(n*n) 
+	int arr[]={4,4,3,2,4};
+	int n=sizeof arr/sizeof arr[0];
+	int maxarea=INT_MIN ,mn;
 
-   for(int i=1;i<=tc;i++)
+	for(int i=1;i<n;i++)
+   {
+      maxarea=max(maxarea,arr[i]);
+      mn=arr[i];
+
+      for(int j=i-1;j>=0;j--)
+      {
+         mn=min(mn,arr[j]);
+         maxarea=max( maxarea,(mn*(i-j+1)));
+      }
+   }
+
+   cout<<maxarea<<endl;
+
+   */
+
+   int q,n;
+
+   cin>>q;
+   for(int i=1;i<=q;i++)
    {
       cin>>n;
-      int hist[30002];
+      int arr[n+2];
 
-     for(int i=0;i<n;i++) cin>>hist[i];
-
-     cout<<"Case "<<i<<": "<<Largest_area(hist,n)<<endl;
-
+      for(int i=0;i<n;i++) cin>>arr[i];
+      cout<<"Case "<<i<<": ";
+      Get_max_area(arr,n);
    }
-  return 0;
+    return 0;
 }
-
-/// Complexity: O(n);
